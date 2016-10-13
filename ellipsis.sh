ea9ca024e007a9971102f51cbd775e8063e3b888
Torgny Bjers <torgny@clear-markets.com>
2016-10-13 14:16:42 -0400

    Added Atom dot files and installation

--- ellipsis.sh
+++ ellipsis.sh
@@ -2,23 +2,38 @@
 #
 # tbjers/dot-atom ellipsis package
 
-# The following hooks can be defined to customize behavior of your package:
-# pkg.install() {
-#     fs.link_files $PKG_PATH
-# }
+pkg.link() {
+  files=(config.cson init.coffee keymap.cson snippets.cson styles.less)
+  for file in ${files[@]}; do
+    fs.link_file common/$file $HOME/.atom/$file
+  done
+}
 
-# pkg.push() {
-#     git.push
-# }
-
-# pkg.pull() {
-#     git.pull
-# }
-
-# pkg.installed() {
-#     git.status
-# }
-#
-# pkg.status() {
-#     git.diffstat
-# }
+pkg.install() {
+  mkdir -p $HOME/.atom
+  case $(os.platform) in
+    osx)
+      if [ -z utils.cmd_exists 'brew' ]; then
+        brew cask install --appdir="/Applications" atom
+      else
+        echo "Please install Homebrew."
+      fi
+      ;;
+    linux)
+      RPM_FILE="`curl -s https://api.github.com/repos/atom/atom/releases | jq '[.[] | select(.prerelease == false)] | [.[] | .assets[] | select(.browser_download_url | endswith(".x86_64.rpm")).browser_download_url][0]' | tr -d '\"'`"
+      RPM_VERSION="`echo $RPM_FILE | grep -o '/v[0-9][^/]\+/' | cut -d '/' -f 2`"
+      PACKAGE="atom-${RPM_VERSION/v/}"
+      if [[ ! -z "`rpm -q $PACKAGE | head -n 1 | grep 'not installed'`" ]]; then
+        sudo dnf install -y --allowerasing pygtk2 libgnome pygpgme "${RPM_FILE}"
+        apm upgrade --no-confirm --no-color \
+          Stylus atom-beautify atom-jade atom-wallaby bottom-dock case-conversion \
+          docblockr git-log language-docker language-scala language-spacebars linter \
+          linter-docker linter-eslint markdown-scroll-sync markdown-writer \
+          merge-conflicts minimap minimap-pigments octocat-syntax open-recent pigments sort-lines \
+          space-tab todo-show wordcount
+      else
+        echo "Atom $RPM_VERSION is already installed, skipping."
+      fi
+      ;;
+  esac
+}
